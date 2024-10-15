614791cb17306240d15ff3403095e94fb33d0202
Augustine Ofor <100804858+Iupac120@users.noreply.github.com>
2024-10-15 10:13:17 +0100

    added origin

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,14 +8,14 @@ const morgan = require("morgan")
 const whiteList = [process.env.CLIENT_URL, process.env.CLIENT2_URL]
 
 const corsOptions = {
-    origin:'https://genrandom.onrender.com',
-    // origin:function (origin,callback){
-    //   if(whiteList.indexOf(origin) !==-1 || !origin){
-    //     callback(null,true)
-    //   }else{
-    //     callback(new Error("Not allowed by CORS"))
-    //   }
-    // } ,
+    
+    origin:function (origin,callback){
+      if(whiteList.indexOf(origin) !==-1 || !origin){
+        callback(null,true)
+      }else{
+        callback(new Error("Not allowed by CORS"))
+      }
+    } ,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
@@ -26,6 +27,15 @@ app.use(express.urlencoded({extended:false}))
 app.use(morgan('tiny'))
 app.use(cors(corsOptions))
 
+app.use((err, req, res, next) => {
+    if (err instanceof cors.CorsError) {
+      // Handle CORS error
+      res.status(400).send({ message: 'CORS Error: Not allowed by CORS' });
+    } else {
+      next(err);
+    }
+  });
+  
 require("./model/SecondaryModel")
 const secTable = require("./controller/tableController")
 
